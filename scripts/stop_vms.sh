21eae394a423dfeb8559a4d888f82cdcce770aab
Mat Schaffer <mat@schaffer.me>
2011-05-12 23:53:25 -0400

    Make stop script wait until VMs actually stop.

--- scripts/stop_vms.sh
+++ scripts/stop_vms.sh
@@ -3,5 +3,14 @@
 stop() {
   VBoxManage controlvm $1 acpipowerbutton
 }
+
+wait_for_stop() {
+  while VBoxManage list runningvms | grep "$1" > /dev/null 2>&1; do
+    sleep 1
+  done
+}
+
 stop MySQLMaster
 stop MySQLSlave
+wait_for_stop MySQLMaster
+wait_for_stop MySQLSlave
